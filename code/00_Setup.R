2893c180555d062080574f3a37b6b209dd34ef70
kushalnavghare <navgharekush@gmail.com>
2023-04-30 12:37:54 -0500

    initial commit

---
+++ code/00_Setup.R
@@ -0,0 +1,21 @@
+###############################################################################
+# Setup
+###############################################################################
+# Setup
+# rm(list=ls())
+library(tidyr)
+library(dplyr)
+library(dtplyr)
+library(ggplot2)
+library(reshape)
+library(caret)
+library(rpart)
+library(kknn)
+library(cluster)
+library(stats)
+library(factoextra)
+library(ggcorrplot)
+library(GGally)
+library(rpart.plot)
+set.seed(143)
+options(scipen = 9999)
